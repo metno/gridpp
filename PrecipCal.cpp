81e17cb70dbc35910e65fc0745aafc657c12b381
Thomas Nipen <tnipen@gmail.com>
2014-12-17 09:57:19 +0100

    Improved description of program

--- PrecipCal.cpp
+++ PrecipCal.cpp
@@ -3,28 +3,27 @@
 #include "DataFile.h"
 #include "ParameterFile.h"
 #include "Calibration.h"
-#include <ctime>
 int main(int argc, const char *argv[]) {
 
    // Parse command line attributes
    if(argc != 4) {
-      std::cout << "Usage:" << std::endl;
-      std::cout << "   precipCalibration.exe <dataFile> <parameterFile> <outputFile>" << std::endl;
-      std::cout << "   - dataFile: Netcdf file with Arome data" << std::endl;
-      std::cout << "   - parameterFile: Text file with training coefficients" << std::endl;
-      std::cout << "   - outputFile: Name of file to write factors" << std::endl;
+      std::cout << "Calibration of ensemble precipitation forecasts" << std::endl;
+      std::cout << std::endl;
+      std::cout << "usage:  precipCalibration.exe input parameters output" << std::endl;
+      std::cout << std::endl;
+      std::cout << "Arguments:" << std::endl;
+      std::cout << "   input         Netcdf file with ECMWF ensemble data" << std::endl;
+      std::cout << "   parameters    Text file with parameters" << std::endl;
+      std::cout << "   output        Netcdf file to write output to" << std::endl;
       return 1;
    }
    std::string dataFile   = argv[1];
    std::string parameterFile = argv[2];
    std::string outputFile = argv[3];
 
-   // Get list of coefficients, one set for each site
-   ParameterFile parameters(parameterFile);
-
-   // Set up data files
    DataFile input(dataFile);
    DataFile output(outputFile);
+   ParameterFile parameters(parameterFile);
 
    Calibration cal(parameters);
    cal.calibrate(input, output);

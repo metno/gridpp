metno/gridpp

commit 46bc0464969056c68a498397dc8f0d07a20d728d

Adds -c altitude

src/File/Arome.cpp
@@ -185,6 +185,7 @@ void FileArome::writeCore(std::vector<Variable::Type> iVariables) {
    startDataMode(); // 0.5 seconds
    writeReferenceTime();
    writeTimes(); // 2-3 seconds
+   writeLatLonVariable("altitude");
    for(int v = 0; v < iVariables.size(); v++) {
       Variable::Type varType = iVariables[v];
       std::string variable = getVariableName(varType);
@@ -363,6 +364,55 @@ vec2 FileArome::getLatLonVariable(std::string iVar) const {
    delete[] values;
    return grid;
 }
+void FileArome::writeLatLonVariable(std::string iVar) {
+   if(!hasVar(iVar)) {
+      // Create variable
+      int dLon     = getDim(getXname());
+      int dLat     = getDim(getYname());
+      int dims[2]  = {dLat, dLon};
+      int var = Util::MV;
+      int status = nc_def_var(mFile, iVar.c_str(), NC_FLOAT, 2, dims, &var);
+      handleNetcdfError(status, "could not define variable");
+   }
+   int var = getVar(iVar);
+   float MV = getMissingValue(var);
+   int numDims;
+   int status = nc_inq_varndims(mFile, var, &numDims);
+   handleNetcdfError(status, "could not determine number of dimensions for variable " + iVar);
+
+   // Assign values
+   float* values = new float[getNumLon()*getNumLat()];
+   vec2 grid;
+   grid.resize(getNumLat());
+   for(int i = 0; i < getNumLat(); i++) {
+      grid[i].resize(getNumLon());
+      for(int j = 0; j < getNumLon(); j++) {
+         int index = i*getNumLon() + j;
+         float value = values[index];
+         if(values[index] == MV)
+            value = Util::MV;
+         grid[i][j] = value;
+         assert(index < getNumLon()*getNumLat());
+      }
+   }
+
+   // Write to file
+   if(numDims == 2) {
+      status = nc_put_var_float(mFile, var, values);
+      handleNetcdfError(status, "could not set data to variable " + iVar);
+   }
+   else if(numDims == 4) {
+      size_t count[4] = {1,1,getNumLat(),getNumLon()};
+      size_t start[4] = {0,0,0,0};
+      int status = nc_put_vara_float(mFile, var, start, count, values);
+      handleNetcdfError(status, "could not set data to variable " + iVar);
+   }
+   else {
+      Util::error("Cannot write " + iVar + " from AROME file. Must have either 2 or 4 dimensions");
+   }
+   delete[] values;
+
+}
 int FileArome::getDate() const {
    return mDate;
 }
@@ -372,7 +422,7 @@ bool FileArome::isValid(std::string iFilename) {
    int file;
    int status = nc_open(iFilename.c_str(), NC_NOWRITE, &file);
    if(status == NC_NOERR) {
-      isValid = hasDim(file, "time") &&
+      isValid = 
                (hasDim(file, "x") || hasDim(file, "rlon")) &&
                (hasDim(file, "y") || hasDim(file, "rlat")) &&
                !hasDim(file, "ensemble_member") &&

src/File/Arome.h
@@ -24,6 +24,7 @@ class FileArome : public FileNetcdf {
       FieldPtr getFieldCore(Variable::Type iVariable, int iTime) const;
       FieldPtr getFieldCore(std::string iVariable, int iTime) const;
       vec2 getLatLonVariable(std::string iVariable) const;
+      void writeLatLonVariable(std::string iVariable);
       int mDate;
       std::string getXname() const;
       std::string getYname() const;

src/Calibrator/Altitude.cpp
@@ -1,0 +1,38 @@
+#include "Altitude.h"
+#include <cmath>
+#include "../Util.h"
+#include "../File/File.h"
+#include "../ParameterFile/ParameterFile.h"
+CalibratorAltitude::CalibratorAltitude(const Options& iOptions) :
+      Calibrator(iOptions) {
+}
+bool CalibratorAltitude::calibrateCore(File& iFile, const ParameterFile* iParameterFile) const {
+   if(iParameterFile == NULL) {
+      Util::error("Calibrator 'altitude' requires a parameter file");
+   }
+
+   if(!iParameterFile->isLocationDependent()) {
+      Util::error("Cannot use a location independent parameter file to update the altitudes");
+   }
+   int nLat = iFile.getNumLat();
+   int nLon = iFile.getNumLon();
+   vec2 lats = iFile.getLats();
+   vec2 lons = iFile.getLons();
+   vec2 elevs = iFile.getElevs();
+
+   #pragma omp parallel for
+   for(int i = 0; i < nLat; i++) {
+      for(int j = 0; j < nLon; j++) {
+         Location loc = iParameterFile->getNearestLocation(0, Location(lats[i][j], lons[i][j], elevs[i][j]));
+         elevs[i][j] = loc.elev();
+      }
+   }
+   iFile.setElevs(elevs);
+   return true;
+}
+
+std::string CalibratorAltitude::description() {
+   std::stringstream ss;
+   ss << Util::formatDescription("-c altitude", "Changes the altitudes in the file to the altitudes in the parameter file.") << std::endl;
+   return ss.str();
+}

src/Calibrator/Altitude.h
@@ -1,0 +1,16 @@
+#ifndef CALIBRATOR_ALTITUDE_H
+#define CALIBRATOR_ALTITUDE_H
+#include "Calibrator.h"
+class ParameterFile;
+class Parameters;
+
+//! Changes the altitudes in iFile to the altitudes in the parameter file
+class CalibratorAltitude : public Calibrator {
+   public:
+      CalibratorAltitude(const Options& iOptions);
+      static std::string description();
+      std::string name() const {return "altitude";};
+   private:
+      bool calibrateCore(File& iFile, const ParameterFile* iParameterFile) const;
+};
+#endif

src/Calibrator/Calibrator.cpp
@@ -152,6 +152,11 @@ Calibrator* Calibrator::getScheme(std::string iName, const Options& iOptions) {
 
       return c;
    }
+   else if(iName == "altitude") {
+      CalibratorAltitude* c = new CalibratorAltitude(iOptions);
+
+      return c;
+   }
    else {
       Util::error("Could not instantiate calibrator with name '" + iName + "'");
       return NULL;
@@ -195,20 +200,21 @@ Parameters Calibrator::train(const std::vector<ObsEns>& iData) const {
 
 std::string Calibrator::getDescriptions() {
    std::stringstream ss;
-   ss << CalibratorZaga::description() << std::endl;
-   ss << CalibratorCloud::description() << std::endl;
-   ss << CalibratorQc::description() << std::endl;
-   ss << CalibratorQq::description() << std::endl;
-   ss << CalibratorQnh::description() << std::endl;
-   ss << CalibratorDiagnose::description() << std::endl;
    ss << CalibratorAccumulate::description() << std::endl;
-   ss << CalibratorWindDirection::description() << std::endl;
+   ss << CalibratorAltitude::description() << std::endl;
+   ss << CalibratorBct::description() << std::endl;
+   ss << CalibratorCloud::description() << std::endl;
+   ss << CalibratorDiagnose::description() << std::endl;
+   ss << CalibratorGaussian::description() << std::endl;
+   ss << CalibratorKriging::description() << std::endl;
    ss << CalibratorNeighbourhood::description() << std::endl;
    ss << CalibratorPhase::description() << std::endl;
+   ss << CalibratorQc::description() << std::endl;
+   ss << CalibratorQnh::description() << std::endl;
+   ss << CalibratorQq::description() << std::endl;
    ss << CalibratorRegression::description() << std::endl;
-   ss << CalibratorKriging::description() << std::endl;
-   ss << CalibratorGaussian::description() << std::endl;
-   ss << CalibratorBct::description() << std::endl;
    ss << CalibratorSort::description() << std::endl;
+   ss << CalibratorWindDirection::description() << std::endl;
+   ss << CalibratorZaga::description() << std::endl;
    return ss.str();
 }

src/Calibrator/Calibrator.h
@@ -40,22 +40,23 @@ class Calibrator : public Scheme {
       virtual bool calibrateCore(File& iFile, const ParameterFile* iParameterFile) const = 0;
    private:
 };
-#include "Zaga.h"
-#include "Cloud.h"
 // #include "Wind.h"
 #include "Temperature.h"
 #include "Accumulate.h"
+#include "Altitude.h"
 #include "Bct.h"
+#include "Cloud.h"
+#include "Diagnose.h"
 #include "Gaussian.h"
+#include "Kriging.h"
 #include "Neighbourhood.h"
+#include "Phase.h"
 #include "Qc.h"
 #include "Qnh.h"
 #include "Qq.h"
-#include "Phase.h"
 #include "Regression.h"
 #include "Sort.h"
 #include "WindDirection.h"
-#include "Kriging.h"
-#include "Diagnose.h"
 #include "Window.h"
+#include "Zaga.h"
 #endif

src/File/File.h
@@ -77,6 +77,7 @@ class File {
       static std::string getDescriptions();
    protected:
       virtual FieldPtr getFieldCore(Variable::Type iVariable, int iTime) const = 0;
+      // File must save variables, but also altitudes, in case they got changed
       virtual void writeCore(std::vector<Variable::Type> iVariables) = 0;
       //! Can the subclass provide this variable?
       virtual bool hasVariableCore(Variable::Type iVariable) const = 0;

src/File/Netcdf.cpp
@@ -64,6 +64,8 @@ int FileNetcdf::getVar(std::string iVar) const {
 }
 
 int FileNetcdf::getDimSize(std::string iDim) const {
+   if(!hasDim(iDim))
+      return 0;
    int dim = getDim(iDim);
    size_t len;
    int status = nc_inq_dimlen(mFile, dim, &len);

commit d684692510e98169d44be62e1c5813148080d74a

Fixes NETCDF error introduced by 46bc046
* The elevation variable should be defined while in define mode

src/File/Arome.cpp
@@ -155,7 +155,14 @@ FileArome::~FileArome() {
 }
 
 void FileArome::writeCore(std::vector<Variable::Type> iVariables) {
+   // General rule: Try to define all variables first, then write variables
+   // to avoid swapping between define and data mode unnecessarily
    startDefineMode();
+
+   // Define lat/lon/elev
+   defineLatLonVariable("latitude");
+   defineLatLonVariable("longitude");
+   defineLatLonVariable("altitude");
 
    // Define variables (< 0.01 s)
    for(int v = 0; v < iVariables.size(); v++) {
@@ -364,7 +371,7 @@ vec2 FileArome::getLatLonVariable(std::string iVar) const {
    delete[] values;
    return grid;
 }
-void FileArome::writeLatLonVariable(std::string iVar) {
+void FileArome::defineLatLonVariable(std::string iVar) {
    if(!hasVar(iVar)) {
       // Create variable
       int dLon     = getDim(getXname());
@@ -373,6 +380,14 @@ void FileArome::writeLatLonVariable(std::string iVar) {
       int var = Util::MV;
       int status = nc_def_var(mFile, iVar.c_str(), NC_FLOAT, 2, dims, &var);
       handleNetcdfError(status, "could not define variable");
+   }
+}
+
+void FileArome::writeLatLonVariable(std::string iVar) {
+   if(!hasVar(iVar)) {
+      startDefineMode();
+      defineLatLonVariable(iVar);
+      startDataMode();
    }
    int var = getVar(iVar);
    float MV = getMissingValue(var);

src/File/Arome.h
@@ -25,6 +25,7 @@ class FileArome : public FileNetcdf {
       FieldPtr getFieldCore(std::string iVariable, int iTime) const;
       vec2 getLatLonVariable(std::string iVariable) const;
       void writeLatLonVariable(std::string iVariable);
+      void defineLatLonVariable(std::string iVariable);
       int mDate;
       std::string getXname() const;
       std::string getYname() const;
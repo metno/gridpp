87c7a70102254f8fa0d9f87a006824eb2b141f6f
Thomas Nipen <tnipen@gmail.com>
2020-09-11 22:28:52 +0200

    Remove advection_implicit_x and advection_implicit_y

--- src/api/advection.cpp
+++ src/api/advection.cpp
@@ -2,60 +2,6 @@
 #include <iostream>
 
 
-vec2 gridpp::advection_implicit_y(const vec2& y_dist, const vec2& x_dist, float dt) {
-    int Y = y_dist.size();
-    int X = y_dist[0].size();
-    vec2 values(Y);
-    for(int y = 0; y < Y; y++) {
-        values[y].resize(X);
-        for(int x = 0; x < X; x++) {
-            // values[y][x] = y_dist[y][x];
-            values[y][x] = gridpp::MV;
-        }
-    }
-    for(int y = 0; y < Y; y++) {
-        for(int x = 0; x < X; x++) {
-            int l = int(y + dt * y_dist[y][x]);
-            int k = int(x + dt * x_dist[y][x]);
-            if(k >= 0 && k < X && l >= 0 && l < Y) {
-                values[l][k] = y;
-            }
-        }
-    }
-    return values;
-}
-vec2 gridpp::advection_implicit_x(const vec2& y_dist, const vec2& x_dist, float dt) {
-    int Y = y_dist.size();
-    int X = y_dist[0].size();
-    vec2 values(Y);
-    ivec2 count(Y);
-    for(int y = 0; y < Y; y++) {
-        values[y].resize(X);
-        count[y].resize(X, 0);
-        for(int x = 0; x < X; x++) {
-            // values[y][x] = x_dist[y][x];
-            values[y][x] = gridpp::MV;
-        }
-    }
-    for(int x = 0; x < X; x++) {
-        for(int y = 0; y < Y; y++) {
-            int l = int(y + dt * y_dist[y][x]);
-            int k = int(x + dt * x_dist[y][x]);
-            if(k >= 0 && k < X && l >= 0 && l < Y) {
-                // std::cout << y << " " << x << " -> " << l << " " << k << std::endl;
-                values[l][k] = x;
-                // count[l][k]++;
-            }
-        }
-    }
-    // for(int x = 0; x < X; x++) {
-    //     for(int y = 0; y < Y; y++) {
-    //         if(count[y][x] > 0)
-    //             values[y][x] /= count[y][x];
-    //     }
-    // }
-    return values;
-}
 vec2 gridpp::advection_implicit(const vec2& y_dist, const vec2& x_dist, float dt, ivec2& y_coord, ivec2& x_coord) {
     int Y = y_dist.size();
     int X = y_dist[0].size();

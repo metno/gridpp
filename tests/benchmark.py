6e4d5ca4428461d1a8db6b2e9a359f3668e45976
Thomas Nipen <tnipen@gmail.com>
2020-11-23 21:48:52 +0100

    Added -s -n -t to benchmark program

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -6,6 +6,9 @@ import gridpp
 def main():
     parser = argparse.ArgumentParser(description='Runs gridpp benchmarks for processing performance')
     parser.add_argument('-j', type=int, help='Run multiple cores', dest='num_cores')
+    parser.add_argument('-s', type=int, default=1, help='Enlarge the inputs by this scaling factor to run a bigger test', dest='scaling')
+    parser.add_argument('-n', type=int, default=1, help='Number of iterations to average over', dest='iterations')
+    parser.add_argument('-t', help='Run only this function', dest="function")
 
     # if len(sys.argv) == 1:
     #     parser.print_help()
@@ -15,21 +18,30 @@ def main():
 
     input = dict()
     grids = dict()
-    for i in [100, 200, 1000, 2000, 10000]:
-        input[i] = np.random.rand(i, i)
-    for i in [100, 200, 1000, 2000]:
-        grids[i] = gridpp.Grid(*np.meshgrid(np.linspace(0, 1, i), np.linspace(0, 1, i)))
+    points = dict()
+
+    for i in [10, 50, 100, 200, 1000, 2000, 10000]:
+        input[i] = np.random.rand(i * args.scaling, i)
+    for i in [10, 50, 100, 200, 1000]:
+        grids[i] = gridpp.Grid(*np.meshgrid(np.linspace(0, 1, i), np.linspace(0, 1, i * args.scaling)))
+    for i in [1000]:
+        # points[i] = gridpp.Points(np.linspace(0, 1, i), np.zeros(i))
+        points[i] = gridpp.Points(np.random.rand(i), np.random.rand(i))
+    structure = gridpp.BarnesStructure(1000)
     radius = 7
     quantile = 0.5
     thresholds = np.linspace(0, 1, 11)
     run = dict()
-    run[(gridpp.Grid, 1000)] = {"expected": 0.74, "args":np.meshgrid(np.linspace(0, 1, 1000), np.linspace(0, 1, 1000))}
-    run[(gridpp.neighbourhood, 10000)] = {"expected": 2.27, "args":(input[10000], radius, gridpp.Mean)}
-    run[(gridpp.neighbourhood,"2000 max")] = {"expected": 2.23, "args":(input[2000], radius, gridpp.Max)}
-    run[(gridpp.neighbourhood_quantile_fast, 2000)] = {"expected": 1.41, "args":(input[2000], quantile, radius, thresholds)}
-    run[(gridpp.bilinear, 200)] = {"expected": 1.43, "args":(grids[200], grids[200], input[200])}
-    run[(gridpp.nearest, 1000)] = {"expected": 1.42, "args":(grids[1000], grids[1000], input[1000])}
+    run[(gridpp.Grid, "1000²")] = {"expected": 0.74, "args":np.meshgrid(np.linspace(0, 1, 1000 * args.scaling), np.linspace(0, 1, 1000))}
+    run[(gridpp.neighbourhood, "10000²")] = {"expected": 2.46, "args":(input[10000], radius, gridpp.Mean)}
+    run[(gridpp.neighbourhood,"2000² max")] = {"expected": 2.23, "args":(input[2000], radius, gridpp.Max)}
+    run[(gridpp.neighbourhood_quantile_fast, "2000²")] = {"expected": 1.54, "args":(input[2000], quantile, radius, thresholds)}
+    run[(gridpp.bilinear, "1000²")] = {"expected": 1.68, "args":(grids[1000], grids[1000], input[1000])}
+    run[(gridpp.nearest, "1000²")] = {"expected": 1.52, "args":(grids[1000], grids[1000], input[1000])}
+    run[(gridpp.optimal_interpolation, "1000² 1000")] = {"expected": 2.47, "args":(grids[1000], input[1000], points[1000], np.zeros(1000), np.ones(1000), np.ones(1000), structure, 10)}
+    run[(gridpp.dewpoint, "1e7")] = {"expected": 0.53, "args":(np.zeros(10000000) + 273.15, np.zeros(10000000))}
 
+    print("Gridpp version %s" % gridpp.version())
     if args.num_cores is not None:
         print("Function                             Expected     Time     Diff    Scaling")
     else:
@@ -39,22 +51,32 @@ def main():
         num_cores += [args.num_cores]
     for key in run.keys()       :
         timings = dict()
+        for num_core in num_cores:
+            timings[num_core] = 0
+
         if isinstance(key, tuple):
             name = key[0].__name__ + " " + str(key[1])
             func = key[0]
         else:
             name = key.__name__
             func = key
+        if args.function is not None:
+            if func.__name__ != args.function:
+                continue
         for num_core in num_cores:
             gridpp.set_omp_threads(num_core)
-            s_time = time.time()
-            func(*run[key]["args"])
-            e_time = time.time()
-            curr_time = e_time - s_time
-            timings[num_core] = curr_time
-            # print("%s() Expected: %.2f s Time: %.2f s" % (func.__name__, run[key]["expected"], e_time - s_time))
-        diff = (timings[1] - run[key]["expected"]) / run[key]["expected"] * 100
-        string = "%-36s %8.2f %8.2f %8.2f %%" % (name, run[key]["expected"], timings[1], diff)
+            for it in range(args.iterations):
+                s_time = time.time()
+                func(*run[key]["args"])
+                e_time = time.time()
+                curr_time = e_time - s_time
+                timings[num_core] += curr_time
+                # print("%s() Expected: %.2f s Time: %.2f s" % (func.__name__, run[key]["expected"], e_time - s_time))
+        for num_core in num_cores:
+            timings[num_core] /= args.iterations
+
+        diff = (timings[1] - run[key]["expected"] * args.scaling) / (run[key]["expected"]  * args.scaling) * 100
+        string = "%-36s %8.2f %8.2f %8.2f %%" % (name, run[key]["expected"] * args.scaling, timings[1], diff)
         if args.num_cores is not None:
             scaling = timings[1] / timings[args.num_cores] / args.num_cores
             expected = timings[1] / args.num_cores

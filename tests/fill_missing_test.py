eb264bf238515886b17bb5e4a3b0698103357ec6
Thomas Nipen <tnipen@gmail.com>
2021-03-19 13:29:53 +0100

    Fixes segmentation fault with fill_missing
    Segmentation fault occurs with fill_missing function in very specific
    conditions, when the Y dimension is shorter than the X dimension, and
    there is a y-slice with missing values on the upper edge.
    
    Thanks to @ssilje for reporting.

--- tests/fill_missing_test.py
+++ tests/fill_missing_test.py
@@ -29,6 +29,14 @@ class Test(unittest.TestCase):
         output = gridpp.fill_missing(values)
         np.testing.assert_array_equal(output, values0)
 
+    def test_missing_on_y_edge(self):
+        """Regression test for bug when X is wider than Y, and a y-slice has missing all the way to the upper edge"""
+        values0 = np.reshape(np.arange(24), [3, 8]).astype(float)
+        values = np.copy(values0)
+        values[1:, 1] = np.nan
+        output = gridpp.fill_missing(values)
+        np.testing.assert_array_equal(output, values0)
+
     def test_missing_on_both_edges(self):
         """Check that we are able to recover the missing values"""
         values0 = np.reshape(np.arange(25), [5, 5]).astype(float)

f53334bb78f2e6f608d5c6e20080492eb1e11f26
Mathias Nipen <jmnipen@gmail.com>
2021-08-31 15:56:20 +0200

    fixed fucntionality in calc neighbourhood

--- src/api/calc_neighbourhood.cpp
+++ src/api/calc_neighbourhood.cpp
@@ -1,33 +1,56 @@
 #include "gridpp.h"
 
+#include <iostream>
+
 using namespace gridpp;
 
-vec2 gridpp::calc_neighbourhood(const vec2& array, const vec2& search_array,int halfwidth, int search_criteria , int search_target){
+vec2 gridpp::calc_neighbourhood(const vec2& array, const vec2& search_array,int halfwidth, 
+    float search_criteria_min, float search_criteria_max , float search_target_min, float search_target_max){
 
+    std::cout << "starting";
 
+    if(search_criteria_min > search_criteria_max){
+        throw std::invalid_argument("Search_criteria_min must be smaller than search_criteria_max");
+    }
+    if(search_target_min > search_target_max){
+        throw std::invalid_argument("Search_target_min must be smaller than search_target_max"); 
+    }
     if(halfwidth < 0){
         throw std::invalid_argument("halfwidth must be positive");
     }
 
+    vec2 output = gridpp::init_vec2(array.size(), array[0].size());
+
     int nY = array.size();
     int nX = array[0].size();
 
-    vec2 output = gridpp::init_vec2(array.size(), array[0].size());
-
+    
     for(int y = 0; y < array.size(); y++){
         for(int x = 0; x < array[y].size(); x++){
+            
             bool start = true;
 
             float current_max = 0;
             int I_maxSearchArray_Y = 0;
             int I_maxSearchArray_X = 0;
 
+            if(search_array[y][x] <= search_criteria_min){
+                output[y][x] = array[y][x];
+                continue;
+            }
+            
+            if(search_array[y][x] > search_target_max){
+                output[y][x] = array[y][x];
+                continue;
+            }
+
             for(int yy = std::max(0, y - halfwidth); yy <= std::min(nY - 1, y + halfwidth); yy++){
                 for(int xx = std::max(0, x - halfwidth); xx <= std::min(nX - 1, x + halfwidth); xx++){
+                    
                     float current_array = search_array[yy][xx];
                     if(!gridpp::is_valid(current_array)){
                         continue;
-                    }
+                    } 
 
                     if(start){
                         start = false;
@@ -41,9 +64,13 @@ vec2 gridpp::calc_neighbourhood(const vec2& array, const vec2& search_array,int 
                         I_maxSearchArray_Y = yy;
                         I_maxSearchArray_X = xx;
                     }
+                    
+                    //std::cout << "inner loop";
+                    
                 }
             }
-
+            
+            
             if(!gridpp::is_valid(current_max)){
                 output[y][x] = 0;
             }
@@ -53,7 +80,8 @@ vec2 gridpp::calc_neighbourhood(const vec2& array, const vec2& search_array,int 
             }
         }
     }
-
+    std::cout << " ends ";
+    return output;
 }
 /*  array = input temperature
 *        search_array = input_lafs

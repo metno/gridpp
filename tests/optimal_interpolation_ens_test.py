e0612726e18dd3c016d0b2523d098fd071b18872
Thomas Nipen <tnipen@gmail.com>
2021-09-15 14:37:59 +0200

    Fixed bug in oi_ensi when some observations are missing
    The nan from the observations would cause nans in the output, for
    gridpoints within the radius of influence.

--- tests/optimal_interpolation_ens_test.py
+++ tests/optimal_interpolation_ens_test.py
@@ -7,15 +7,31 @@ import collections
 
 class Test(unittest.TestCase):
     def test_no_obs(self):
+        """ Check that we get the same field back if there are no observations """
         grid = gridpp.Points([0], [0])
+        E = 3
         points = gridpp.Points([], [])
-        pratios = []
-        structure = gridpp.BarnesStructure(500)
+        psigmas = []
+        structure = gridpp.BarnesStructure(500000)
         pobs = []
-        background = np.zeros(grid.size())
-        pbackground = []
+        background = np.zeros([grid.size(), E])
+        pbackground = np.zeros([0, E])
         max_points = 10
-        output0 = gridpp.optimal_interpolation(grid, background, points, pobs, pratios, pbackground, structure, max_points)
+        output0 = gridpp.optimal_interpolation_ensi(grid, background, points, pobs, psigmas, pbackground, structure, max_points)
+        np.testing.assert_almost_equal(output0, background)
+
+    def test_some_missing_obs(self):
+        """ Check that if one observation is missing, that the whole output isn't nan """
+        grid = gridpp.Points([0], [0])
+        E = 3
+        points = gridpp.Points([0, 0.1], [0, 0.1])
+        psigmas = [1, 1]
+        structure = gridpp.BarnesStructure(500000)
+        pobs = [np.nan, 0]
+        background = np.zeros([grid.size(), E])
+        pbackground = np.zeros([points.size(), E])
+        max_points = 10
+        output0 = gridpp.optimal_interpolation_ensi(grid, background, points, pobs, psigmas, pbackground, structure, max_points)
         np.testing.assert_almost_equal(output0, background)
 
 

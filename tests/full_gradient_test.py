0d303ff25b0797c943ae16d0d72d3b91aedafab1
Mathias Nipen <jmnipen@gmail.com>
2021-09-27 11:12:36 +0200

    added grid to points tests

--- tests/full_gradient_test.py
+++ tests/full_gradient_test.py
@@ -21,24 +21,38 @@ class Test(unittest.TestCase):
         self.igrid = gridpp.Grid(self.ilats, self.ilons, self.ielevs, self.ilafs)
         self.ogrid = gridpp.Grid(self.olats, self.olons, self.oelevs, self.olafs)
 
+        self.opoints = gridpp.Points([10,10,10, 15, 15, 15, 20, 20, 20], [40, 45, 50, 40, 45, 50, 40, 45, 50], [0,100,0,0,200,0,0,100,0], [0,1,0,0,1,0,0,1,0])
+
+    def test_grid_to_grid_all_2d(self):
+        output = gridpp.full_gradient(self.igrid, self.ogrid, self.ivalues, self.elev_gradient, self.laf_gradient)        
+        np.testing.assert_array_equal(output, [[10,19,10], [10,18,10], [10,19,10]])
+
+    def test_grid_to_point_all_1d(self):
+        output = gridpp.full_gradient(self.igrid, self.opoints, self.ivalues, self.elev_gradient, self.laf_gradient) 
+        np.testing.assert_array_equal(output, [10,19,10,10,18,10,10,19,10])
+
+
+        """Testing output using LAF parameter  (elevation excluded)"""
     def test_grid_to_grid_laf_2d(self):
         self.elev_gradient[:,:] = 0
-
         output = gridpp.full_gradient(self.igrid, self.ogrid, self.ivalues, self.elev_gradient, self.laf_gradient)
-
         np.testing.assert_array_equal(output, [[10,20,10]]*3)
 
     def test_grid_to_grid_elev_2d(self):
         self.laf_gradient[:,:] = 0
-
         output = gridpp.full_gradient(self.igrid, self.ogrid, self.ivalues, self.elev_gradient, self.laf_gradient)
-
         np.testing.assert_array_equal(output, [[15,14,15], [15,13,15], [15,14,15]])
 
-    def test_grid_to_grid_all_2d(self):
-        output = gridpp.full_gradient(self.igrid, self.ogrid, self.ivalues, self.elev_gradient, self.laf_gradient)        
+        """ Testing output using Elevation paramter  (LAF excluded)"""
+    def test_grid_to_point_laf_1d(self):       
+        self.elev_gradient[:,:] = 0
+        output = gridpp.full_gradient(self.igrid, self.opoints, self.ivalues, self.elev_gradient, self.laf_gradient) 
+        np.testing.assert_array_equal(output, [10,20,10,10,20,10,10,20,10])
 
-        np.testing.assert_array_equal(output, [[10,19,10], [10,18,10], [10,19,10]])
+    def test_grid_to_point_elev_1d(self):
+        self.laf_gradient[:,:] = 0 
+        output = gridpp.full_gradient(self.igrid, self.opoints, self.ivalues, self.elev_gradient, self.laf_gradient) 
+        np.testing.assert_array_equal(output, [15,14,15,15,13,15,15,14,15])
 
 if __name__ == '__main__':
     unittest.main()

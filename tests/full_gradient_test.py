0dbcbee25fc0bb369ba1ea47d833da4c80c25482
Mathias Nipen <jmnipen@gmail.com>
2021-09-28 11:07:14 +0200

    added testing for grid to grid 3d

--- tests/full_gradient_test.py
+++ tests/full_gradient_test.py
@@ -18,6 +18,10 @@ class Test(unittest.TestCase):
         self.elev_gradient = - np.ones(self.ilats.shape) / 100.0
         self.laf_gradient = np.ones(self.ilats.shape) * 10
 
+        self.ivalues3d = np.ones([5,2,2])*15
+        self.elev_gradient3d = - np.ones([5,2,2]) / 100.0 
+        self.laf_gradient3d = np.ones([5,2,2]) * 10
+
         self.igrid = gridpp.Grid(self.ilats, self.ilons, self.ielevs, self.ilafs)
         self.ogrid = gridpp.Grid(self.olats, self.olons, self.oelevs, self.olafs)
 
@@ -31,8 +35,7 @@ class Test(unittest.TestCase):
         output = gridpp.full_gradient(self.igrid, self.opoints, self.ivalues, self.elev_gradient, self.laf_gradient) 
         np.testing.assert_array_equal(output, [10,19,10,10,18,10,10,19,10])
 
-
-        """Testing output using LAF parameter  (elevation excluded)"""
+        """Testing output using LAF parameter (elevation excluded)"""
     def test_grid_to_grid_laf_2d(self):
         self.elev_gradient[:,:] = 0
         output = gridpp.full_gradient(self.igrid, self.ogrid, self.ivalues, self.elev_gradient, self.laf_gradient)
@@ -43,7 +46,7 @@ class Test(unittest.TestCase):
         output = gridpp.full_gradient(self.igrid, self.ogrid, self.ivalues, self.elev_gradient, self.laf_gradient)
         np.testing.assert_array_equal(output, [[15,14,15], [15,13,15], [15,14,15]])
 
-        """ Testing output using Elevation paramter  (LAF excluded)"""
+        """ Testing output using Elevation paramter (LAF excluded)"""
     def test_grid_to_point_laf_1d(self):       
         self.elev_gradient[:,:] = 0
         output = gridpp.full_gradient(self.igrid, self.opoints, self.ivalues, self.elev_gradient, self.laf_gradient) 
@@ -53,6 +56,71 @@ class Test(unittest.TestCase):
         self.laf_gradient[:,:] = 0 
         output = gridpp.full_gradient(self.igrid, self.opoints, self.ivalues, self.elev_gradient, self.laf_gradient) 
         np.testing.assert_array_equal(output, [15,14,15,15,13,15,15,14,15])
+
+    def test_grid_to_grid_laf_3d(self):
+        ivalues = np.ones([5,2,2])*15
+        elev_gradient = np.zeros([5,2,2])
+        laf_gradient = np.ones([5,2,2])*10
+        
+        #laf gradient varying in time
+        laf_gradient2 = np.ones([5,2,2])*10
+        for i in range(len(laf_gradient2[:,0,0])):
+            laf_gradient2[i,:,:] = 10 * (1 - i/5.0)
+
+        #Testing constant laf_gradient through time
+        output = gridpp.full_gradient(self.igrid, self.ogrid, ivalues, elev_gradient, laf_gradient) 
+        np.testing.assert_array_equal(output, [[[10,20,10], [10,20,10], [10,20,10]]]*5)
+
+        #Testing varying laf_gradient through time
+        output = gridpp.full_gradient(self.igrid, self.ogrid, ivalues, elev_gradient, laf_gradient2) 
+        np.testing.assert_array_equal(output, [[[10,20,10], [10,20,10], [10,20,10]], 
+                                            [[11,19,11], [11,19,11], [11,19,11]], 
+                                            [[12,18,12], [12,18,12], [12,18,12]],
+                                            [[13,17,13], [13,17,13], [13,17,13]],
+                                            [[14,16,14], [14,16,14], [14,16,14]]])
+
+    def test_grid_to_grid_elev_3d(self):
+        ivalues = np.ones([5,2,2])*15
+        elev_gradient = - np.ones([5,2,2]) /100.0 
+        laf_gradient = np.zeros([5,2,2])
+
+        elev_gradient2 = np.ones([5,2,2])
+        for i in range(len(elev_gradient2[:,0,0])):
+            elev_gradient2[i,:,:]  = - 0.01 * (i + 1)
+
+        output = gridpp.full_gradient(self.igrid, self.ogrid, ivalues, elev_gradient, laf_gradient)
+        np.testing.assert_array_equal(output,  [[[15,14,15], [15,13,15], [15,14,15]]] * 5 )
+
+        output = gridpp.full_gradient(self.igrid, self.ogrid, ivalues, elev_gradient2, laf_gradient)
+        np.testing.assert_array_equal(output,  [[[15,14,15], [15,13,15], [15,14,15]], 
+                                            [[15,13,15], [15,11,15], [15,13,15]], 
+                                            [[15,12,15], [15,9,15], [15,12,15]], 
+                                            [[15,11,15], [15,7,15], [15,11,15]], 
+                                            [[15,10,15], [15,5,15], [15,10,15]]])
+
+    def test_grid_to_grid_all_3d(self):
+        ivalues = np.ones([5,2,2])*15
+        elev_gradient = - np.ones([5,2,2]) / 100.0 
+        laf_gradient = np.ones([5,2,2]) * 10
+
+        output = gridpp.full_gradient(self.igrid, self.ogrid, ivalues, elev_gradient, laf_gradient)
+
+        np.testing.assert_array_equal(output, [[[10,19,10], [10,18,10], [10,19,10]]] * 5)
+
+        laf_gradient2 = np.ones([5,2,2])*10
+        for i in range(len(laf_gradient2[:,0,0])):
+            laf_gradient2[i,:,:] = 10 * (1 - i/5.0) 
+        elev_gradient2 = np.ones([5,2,2])
+        for i in range(len(elev_gradient2[:,0,0])):
+            elev_gradient2[i,:,:]  = - 0.01 * (i + 1) 
+
+        output = gridpp.full_gradient(self.igrid, self.ogrid, ivalues, elev_gradient2, laf_gradient2)
+        np.testing.assert_array_equal(output,  [[[10,19,10], [10,18,10], [10,19,10]], 
+                                            [[11,17,11], [11,15,11], [11,17,11]], 
+                                            [[12,15,12], [12,12,12], [12,15,12]], 
+                                            [[13,13,13], [13,9,13], [13,13,13]], 
+                                            [[14,11,14], [14,6,14], [14,11,14]]])
+
 
 if __name__ == '__main__':
     unittest.main()

e0700502da0b8346e6f4ae439737d9c0b0fafc8a
lonnekevb <lonnekevb@met.no>
2022-07-05 11:47:07 +0000

    Add more test for downscale_probability and addapt for NaN values

--- src/api/downscale_probability.cpp
+++ src/api/downscale_probability.cpp
@@ -16,8 +16,6 @@ vec2 gridpp::downscale_probability(const Grid& igrid, const Grid& ogrid, const v
     for(int i = 0; i < nLat; i++)
         output[i].resize(nLon);
 
-    // std::cout << ivalues.size() << std::endl;
-
     #pragma omp parallel for collapse(2)
     for(int i = 0; i < nLat; i++) {
         for(int j = 0; j < nLon; j++) {
@@ -25,28 +23,41 @@ vec2 gridpp::downscale_probability(const Grid& igrid, const Grid& ogrid, const v
             int I = indices[0];
             int J = indices[1];
             int count = 0;
+            int total = 0;
             for(int k = 0; k < nEns; k++){
-                if (comparisonoperator == gridpp::leq) {
-                    if (ivalues[k][I][J] <= threshold[i][j]) {
-                        count = count + 1;
-                    }
-                } else if (comparisonoperator == gridpp::lt) {
-                    if (ivalues[k][I][J] < threshold[i][j]) {
-                        count = count + 1;
-                    }
-                } else if (comparisonoperator == gridpp::geq) {
-                    if (ivalues[k][I][J] >= threshold[i][j]) {
-                        count = count + 1;
-                    }
-                } else if (comparisonoperator == gridpp::gt) {
-                    if (ivalues[k][I][J] > threshold[i][j]) {
-                        count = count + 1;
-                    }
-                }
-            // std::cout << "ivalues[" << k << "][" << I << "][" << J << "]:" << ivalues[k][I][J] << std::endl;
-            // std::cout << "threshold[" << i << "][" << j << "]:" << threshold[i][j] << std::endl;                
+                if (gridpp::is_valid(ivalues[k][I][J])) {
+                    count = count + 1;
+                    if (comparisonoperator == gridpp::leq) {
+                        if (ivalues[k][I][J] <= threshold[i][j]) {
+                            total = total + 1;
+                        }
+                    } else if (comparisonoperator == gridpp::lt) {
+                        if (ivalues[k][I][J] < threshold[i][j]) {
+                            total = total + 1;
+                        }
+                    } else if (comparisonoperator == gridpp::geq) {
+                        if (ivalues[k][I][J] >= threshold[i][j]) {
+                            total = total + 1;
+                        }
+                    } else if (comparisonoperator == gridpp::gt) {
+                        if (ivalues[k][I][J] > threshold[i][j]) {
+                            total = total + 1;
+                        }
+                    }   
+                }       
             }
-            float prob = (float)count / (float)nEns;
+            std::cout << "total[" << i <<"][" << j << "]:" << total << std::endl;
+            std::cout << "count[" << i <<"][" << j << "]:" << count << std::endl;
+
+            float prob;
+            if (count == 0) {
+                prob = NAN;
+            }
+            else {
+                prob = (float)total / (float)count;
+                std::cout << "prob[" << i <<"][" << j << "]:" << prob << std::endl;
+            }
+
             output[i][j] = prob;
         }
     }
--- tests/downscale_probability_test.py
+++ tests/downscale_probability_test.py
@@ -5,17 +5,33 @@ import numpy as np
 
 
 class Test(unittest.TestCase):
-    def test_downscale_probability_leq(self):
+    def setUp(self) -> None:
         lons1, lats1 = np.meshgrid([10, 30], [50, 30])
         lons2, lats2 = np.meshgrid([5, 15, 25], [45, 35, 25])
-        grid1 = gridpp.Grid(lats1, lons1)
-        grid2 = gridpp.Grid(lats2, lons2)
-        values = np.array([ [[-1., -1.],[-1., -1.]], 
+        self.grid1 = gridpp.Grid(lats1, lons1)
+        self.grid2 = gridpp.Grid(lats2, lons2)
+        self.values = np.array([ [[-1., -1.],[-1., -1.]], 
                             [[0., 0.],  [0., 0.]], 
                             [[1., 1.],  [1., 1.]]
                             ])
-        thresholds = np.array([ [-2., -0.5, 0.5],
+        self.thresholds = np.array([ [-2., -0.5, 0.5],
                                 [0., 1., -1.], 
                                 [2., 0.5, 0.]])
-        output = gridpp.downscale_probability(grid1, grid2, values, thresholds, gridpp.leq)
+
+    def test_downscale_probability_leq(self):
+        output = gridpp.downscale_probability(self.grid1, self.grid2, self.values, self.thresholds, gridpp.leq)
         np.testing.assert_array_equal(output, np.array([[0., 1./3., 2./3.], [2./3., 1., 1./3.], [1., 2./3., 2./3.]], dtype=np.float32))
+
+    def test_downscale_probability_gt(self):
+        output = gridpp.downscale_probability(self.grid1, self.grid2, self.values, self.thresholds, gridpp.gt)
+        np.testing.assert_array_equal(output, np.array([[1., 2./3., 1./3.], [1./3., 0., 2./3.], [0., 1./3., 1./3.]], dtype=np.float32))
+
+    def test_downscale_probability_geq(self):
+        self.values[0, 1, 1] = np.NaN
+        output = gridpp.downscale_probability(self.grid1, self.grid2, self.values, self.thresholds, gridpp.geq)
+        np.testing.assert_array_equal(output, np.array([[1., 2./3., 1./3.], [2./3., 1./3., 1.], [0., 1./3., 1.]], dtype=np.float32))
+
+    def test_downscale_probability_lt(self):
+        self.values[:, 0, 0] = np.NaN
+        output = gridpp.downscale_probability(self.grid1, self.grid2, self.values, self.thresholds, gridpp.lt)
+        np.testing.assert_array_equal(output, np.array([[np.NaN, np.NaN, 2./3.], [1./3., 2./3., 0.], [1., 2./3., 1./3.]], dtype=np.float32))

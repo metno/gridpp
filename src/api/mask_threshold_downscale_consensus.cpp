5217785cac229466b315f9c8b73c51e9f2f7cba2
Thomas Nipen <tnipen@gmail.com>
2023-01-24 20:30:03 +0100

    Remove ::mask_threshold_downscale_quantile
    Since it is not used. Also renamed ::mask_threshold_downscale_quantile_consensus
    to ::mask_threshold_downscale

--- src/api/mask_threshold_downscale_consensus.cpp
+++ src/api/mask_threshold_downscale_consensus.cpp
@@ -5,19 +5,18 @@
 using namespace gridpp;
 
 namespace{
-    vec2 mask_threshold_downscale_consensus(const Grid& igrid, const Grid& ogrid, const vec3& ivalues_true, const vec3& ivalues_false, const vec3& threshold_values, const vec2& threshold, const ComparisonOperator& comparison_operator, const Statistic& statistic, const float quantile);
-    vec2 mask_threshold_downscale_quantile(const Grid& igrid, const Grid& ogrid, const vec3& ivalues_true, const vec3& ivalues_false, const vec3& threshold_values, const vec2& threshold, const ComparisonOperator& comparison_operator, const Statistic& statistic, const float quantile);
+    vec2 mask_threshold_downscale(const Grid& igrid, const Grid& ogrid, const vec3& ivalues_true, const vec3& ivalues_false, const vec3& threshold_values, const vec2& threshold, const ComparisonOperator& comparison_operator, const Statistic& statistic, const float quantile);
 }
 
 vec2 gridpp::mask_threshold_downscale_consensus(const Grid& igrid, const Grid& ogrid, const vec3& ivalues_true, const vec3& ivalues_false, const vec3& threshold_values, const vec2& threshold, const ComparisonOperator& comparison_operator, const Statistic& statistic) {
-    return ::mask_threshold_downscale_consensus(igrid, ogrid, ivalues_true, ivalues_false, threshold_values, threshold, comparison_operator, statistic, 0);
+    return ::mask_threshold_downscale(igrid, ogrid, ivalues_true, ivalues_false, threshold_values, threshold, comparison_operator, statistic, 0);
 }
 vec2 gridpp::mask_threshold_downscale_quantile(const Grid& igrid, const Grid& ogrid, const vec3& ivalues_true, const vec3& ivalues_false, const vec3& threshold_values, const vec2& threshold, const ComparisonOperator& comparison_operator, const float quantile) {
-    return ::mask_threshold_downscale_consensus(igrid, ogrid, ivalues_true, ivalues_false, threshold_values, threshold, comparison_operator, gridpp::Quantile, quantile);
+    return ::mask_threshold_downscale(igrid, ogrid, ivalues_true, ivalues_false, threshold_values, threshold, comparison_operator, gridpp::Quantile, quantile);
 }
 
 namespace {
-    vec2 mask_threshold_downscale_consensus(const Grid& igrid, const Grid& ogrid, const vec3& ivalues_true, const vec3& ivalues_false, const vec3& threshold_values, const vec2& threshold, const ComparisonOperator& comparison_operator, const Statistic& statistic, const float quantile) {
+    vec2 mask_threshold_downscale(const Grid& igrid, const Grid& ogrid, const vec3& ivalues_true, const vec3& ivalues_false, const vec3& threshold_values, const vec2& threshold, const ComparisonOperator& comparison_operator, const Statistic& statistic, const float quantile) {
         vec2 iOutputLats = ogrid.get_lats();
         vec2 iOutputLons = ogrid.get_lons();
 
@@ -37,7 +36,7 @@ namespace {
                 int J = indices[1];
                 int count = 0;
                 vec masked_ivalues(nEns, gridpp::MV);
-                for(int k = 0; k < nEns; k++){
+                for(int k = 0; k < nEns; k++) {
                     if (gridpp::is_valid(threshold_values[I][J][k] )) {
                         if (comparison_operator == gridpp::Leq) {
                             if (threshold_values[I][J][k] <= threshold[i][j]) {
@@ -67,7 +66,7 @@ namespace {
                             else {
                                 masked_ivalues[k] = ivalues_false[I][J][k];
                             }
-                        }    
+                        }
                     }
 
                 }

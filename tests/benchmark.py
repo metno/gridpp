2051a751016c9f57dffb7eb41a66b926e039ffcd
Thomas Nipen <tnipen@gmail.com>
2024-03-07 13:23:08 +0100

    Improve startup time of benchmark script

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -16,68 +16,65 @@ def main():
     if args.num_cores is not None and args.num_cores < 2:
         raise Exception("Error: Number of cores must be 2 or more")
 
-    input = dict()
-    grids = dict()
-    points = dict()
+    ss_time = time.time()
+
     np.random.seed(1000)
 
     version_major = int(gridpp.version().split('.')[0])
     version_minor = int(gridpp.version().split('.')[1])
 
-    for i in [10, 50, 100, 200, 500, 1000, 2000, 10000]:
-        input[i] = np.random.rand(int(i * args.scaling), i)*10
-    for i in [10, 50, 100, 200, 500, 1000]:
-        grids[i] = gridpp.Grid(*np.meshgrid(np.linspace(0, 1, i), np.linspace(0, 1, int(i * args.scaling))))
-    for i in [1000, 100000]:
-        points[i] = gridpp.Points(np.random.rand(i), np.random.rand(i))
+    # Commonly used inputs
     structure = gridpp.BarnesStructure(10000)
     radius = 7
     quantile = 0.5
     thresholds = np.linspace(0, 1, 11)
-    run = collections.OrderedDict()
-    run[("Grid", "1000²")] = {"expected": 0.74, "args":np.meshgrid(np.linspace(0, 1, int(1000 * args.scaling)), np.linspace(0, 1, 1000))}
-    run[("neighbourhood", "10000²")] = {"expected": 2.05, "args":(np.zeros([10000, 10000]), radius, gridpp.Mean)}
-    run[("neighbourhood","2000² max")] = {"expected": 0.99, "args":(input[2000], radius, gridpp.Max)}
-    run[("neighbourhood_quantile_fast", "2000²")] = {"expected": 1.23, "args":(input[2000], quantile, radius, thresholds)}
-    run[("neighbourhood_quantile", "500²")] = {"expected": 1.70, "args":(input[500], quantile, radius)}
-    run[("bilinear", "1000²")] = {"expected": 1.68, "args":(grids[1000], grids[1000], input[1000])}
-    run[("bilinear", "1000² x 50")] = {"expected": 4.42, "args":(grids[1000], grids[1000], np.repeat(np.expand_dims(input[1000], 0), 50, axis=0))}
-    run[("nearest", "1000²")] = {"expected": 1.52, "args":(grids[1000], grids[1000], np.zeros([1000,1000]))}
-    run[("nearest", "1000² x 50")] = {"expected": 1.93, "args":(grids[1000], grids[1000], np.repeat(np.expand_dims(input[1000], 0), 50, axis=0))}
-    run[("gridding", "200² 100000")] = {"expected": 0.61, "args":(grids[200], points[100000], np.zeros([100000]), 5000, 1, gridpp.Mean)}
-    run[("gridding_nearest", "200² 100000")] = {"expected": 0.11, "args":(grids[200], points[100000], np.zeros([100000]), 1, gridpp.Mean)}
-    run[("optimal_interpolation", "100² 1000")] = {"expected": 0.80, "args":(grids[100],
-        input[100], points[1000], np.zeros(1000), np.ones(1000), np.ones(1000), structure, 20)}
-    run[("dewpoint", "1e7")] = {"expected": 0.53, "args":(np.zeros(10000000) + 273.15, np.zeros(10000000))}
-    run[("fill", "1e5")] = {"expected": 1.96, "args":(grids[200], np.zeros([200, 200]),
-        points[100000], np.ones(100000) * 5000, 1, False)}
-    run[("doping_square", "1e5")] = {"expected": 0.12, "args":(grids[200], np.zeros([200, 200]),
-        points[100000], np.ones(100000) * 1, np.ones(100000, 'int') * 5, False)}
-    run[("doping_circle", "1e5")] = {"expected": 2.00, "args":(grids[200], np.zeros([200, 200]),
-        points[100000], np.ones(100000) * 1, np.ones(100000) * 5000, False)}
-    run[("local_distribution_correction", "")] = {"expected": 1.31, "args":(grids[200], np.zeros([200, 200]),
-        points[1000], np.ones(1000) * 1, np.ones(1000) * 1, structure, 0.1, 0.9, 5)}
-    run[("full_gradient", "1000²")] = {"expected": 1.59, "args": (grids[1000], grids[1000],
-        np.zeros([1000,1000]), np.zeros([1000,1000]), np.zeros([1000,1000]))}
-    if version_major > 0 or version_minor > 6:
-        run[("calc_gradient", "2000²")] = {"expected": 0.45, "args": (np.random.rand(2000, 2000) *
-            100, np.zeros([2000,2000]), gridpp.LinearRegression, 10, 0, 100, 0)}
-        run[("mask_threshold_downscale_consensus", "")] = {"expected": 0.91, "args":(grids[100], grids[1000], np.random.rand(100, 100, 10), np.random.rand(100, 100, 10), np.random.rand(1000, 1000, 10), np.random.rand(1000, 1000), gridpp.Lt, gridpp.Mean)}
-    run[("neighbourhood_search", "2000² 7x7")] = {"expected": 1.11, "args": (np.random.rand(2000, 2000),
-        np.random.rand(2000, 2000), 3, 0.7, 1, 0.1, np.random.rand(2000, 2000) < 0.5)}
-    run[("window", "1000²")] = {"expected": 1.67, "args": (np.random.rand(100000, 1000), 101, gridpp.Mean, False, False)}
-    run[("gamma_inv", "5*201*476")] = {"expected": 1.168, "args": (np.random.rand(5*201*476)*0.9 + 0.05,
-        np.random.rand(5*201*476), np.random.rand(5*201*476))}
-    run[("apply_curve", "")] = {"expected": 0.06, "args": (np.random.rand(2000, 2000),
-        np.random.rand(2000), np.random.rand(2000), gridpp.OneToOne,
-        gridpp.OneToOne)}
-    x = np.sort(np.random.rand(2000, 2000, 10).astype(np.float32), axis=2)
-    y = np.sort(np.random.rand(2000, 2000, 10).astype(np.float32), axis=2)
-    run[("apply_curve", "gridded")] = {"expected": 0.87, "args": (np.random.rand(2000,
-        2000).astype(np.float32), x, y, gridpp.OneToOne, gridpp.OneToOne)}
-    run[("test_vec3_input")] = {"expected": 0.35, "args": (np.zeros([2000, 2000, 10],
-        np.float32),)}
+    x = Input([2000, 2000, 10], args.scaling, sort_axis=2)
+    y = Input([2000, 2000, 10], args.scaling, sort_axis=2)
+    I1000 = Input([1000, 1000], args.scaling)
+    Z1000 = Input([1000, 1000], args.scaling, random=False)
+    I2000 = Input([2000, 2000], args.scaling)
+    G1000 = Grid(1000, args.scaling)
+    cache = Cache()
 
+    """
+    Define benchmarks, including their inputs and their expected runtime. For the inputs, use
+    Computable objects to prevent expensive objects to be generated for all tests. These are then
+    computed on the fly. If any other objects are used, these will be generated when the benchmark
+    is defined. The goal here is to reduce the time it takes to initialize this program.
+
+    The key is (function name to be tested, extra information for the test)
+    The value is a dictionary with the following keys:
+        expected (float): Expected runtime
+        args (list): List of input arguments to the test
+    """
+    run = collections.OrderedDict()
+    run[("Grid", "1000²")] = { "expected": 0.74, "args": (I1000, I1000)}
+    run[("neighbourhood", "10000²")] = {"expected": 2.05, "args":(np.zeros([10000, 10000]), radius, gridpp.Mean)}
+    run[("neighbourhood","2000² max")] = {"expected": 0.99, "args":(I2000, radius, gridpp.Max)}
+    run[("neighbourhood_quantile_fast", "2000²")] = {"expected": 1.23, "args":(I2000, quantile, radius, thresholds)}
+    run[("neighbourhood_quantile", "500²")] = {"expected": 1.70, "args":(Input([500, 500], args.scaling), quantile, radius)}
+    run[("bilinear", "1000²")] = {"expected": 1.68, "args":(G1000, G1000, I1000)}
+    run[("bilinear", "1000² x 50")] = {"expected": 4.42, "args":(G1000, G1000, Input([50, 1000, 1000], args.scaling))}
+    run[("nearest", "1000²")] = {"expected": 1.52, "args":(G1000, G1000, I1000)}
+    run[("nearest", "1000² x 50")] = {"expected": 1.93, "args":(G1000, G1000, Input([50, 1000, 1000], args.scaling))}
+    run[("gridding", "200² 100000")] = {"expected": 0.61, "args":(Grid(200, args.scaling), Points(100000, args.scaling), np.zeros([100000]), 5000, 1, gridpp.Mean)}
+    run[("gridding_nearest", "200² 100000")] = {"expected": 0.11, "args":(Grid(200, args.scaling), Points(100000, args.scaling), np.zeros([100000]), 1, gridpp.Mean)}
+    run[("optimal_interpolation", "100² 1000")] = {"expected": 0.80, "args":(Grid(100, args.scaling), Input([100, 100], args.scaling), Points(1000, args.scaling), np.zeros(1000), np.ones(1000), np.ones(1000), structure, 20)}
+    run[("dewpoint", "1e7")] = {"expected": 0.53, "args":(np.zeros(10000000) + 273.15, np.zeros(10000000))}
+    run[("fill", "1e5")] = {"expected": 1.96, "args":(Grid(200, args.scaling), np.zeros([200, 200]), Points(100000, args.scaling), np.ones(100000) * 5000, 1, False)}
+    run[("doping_square", "1e5")] = {"expected": 0.12, "args":(Grid(200, args.scaling), np.zeros([200, 200]), Points(100000, args.scaling), np.ones(100000) * 1, np.ones(100000, 'int') * 5, False)}
+    run[("doping_circle", "1e5")] = {"expected": 2.00, "args":(Grid(200, args.scaling), np.zeros([200, 200]), Points(100000, args.scaling), np.ones(100000) * 1, np.ones(100000) * 5000, False)}
+    run[("local_distribution_correction", "")] = {"expected": 1.31, "args":(Grid(200, args.scaling), np.zeros([200, 200]), Points(1000, args.scaling), np.ones(1000) * 1, np.ones(1000) * 1, structure, 0.1, 0.9, 5)}
+    run[("full_gradient", "1000²")] = {"expected": 1.59, "args": (G1000, G1000, I1000, I1000, I1000)}
+    if version_major > 0 or version_minor > 6:
+        run[("calc_gradient", "2000²")] = {"expected": 0.45, "args": (np.random.rand(2000, 2000) * 100, np.zeros([2000,2000]), gridpp.LinearRegression, 10, 0, 100, 0)}
+        run[("mask_threshold_downscale_consensus", "")] = {"expected": 0.91, "args":(Grid(100, args.scaling), G1000, np.random.rand(100, 100, 10), np.random.rand(100, 100, 10), np.random.rand(1000, 1000, 10), np.random.rand(1000, 1000), gridpp.Lt, gridpp.Mean)}
+    run[("neighbourhood_search", "2000² 7x7")] = {"expected": 1.11, "args": (np.random.rand(2000, 2000), np.random.rand(2000, 2000), 3, 0.7, 1, 0.1, np.random.rand(2000, 2000) < 0.5)}
+    run[("window", "1000²")] = {"expected": 1.67, "args": (np.random.rand(100000, 1000), 101, gridpp.Mean, False, False)}
+    run[("gamma_inv", "5*201*476")] = {"expected": 1.168, "args": (np.random.rand(5*201*476)*0.9 + 0.05, np.random.rand(5*201*476), np.random.rand(5*201*476))}
+    run[("apply_curve", "")] = {"expected": 0.06, "args": (I2000, np.random.rand(2000), np.random.rand(2000), gridpp.OneToOne, gridpp.OneToOne)}
+    run[("apply_curve", "gridded")] = {"expected": 0.87, "args": (I2000, x, y, gridpp.OneToOne, gridpp.OneToOne)}
+    run[("test_vec3_input")] = {"expected": 0.35, "args": (np.zeros([2000, 2000, 10], np.float32),)}
 
     if args.num_cores is not None:
         print("Gridpp parallelization test (gridpp version %s)" % gridpp.version())
@@ -92,6 +89,7 @@ def main():
     num_cores = [1]
     if args.num_cores is not None:
         num_cores += [args.num_cores]
+
     for key in run.keys()       :
         try:
             timings = dict()
@@ -113,12 +111,29 @@ def main():
             for num_core in num_cores:
                 gridpp.set_omp_threads(num_core)
                 for it in range(args.iterations):
+                    # Evaluate Computable inputs
+                    input_args = list()
+                    for i, k in enumerate(run[key]["args"]):
+                        if issubclass(type(k), Computable):
+                            k = cache.add(k)
+                        elif isinstance(k, list):
+                            inner = list()
+                            for j in k:
+                                if issubclass(type(j), Computable):
+                                    j = cache.add(j)
+                                inner += k
+                            k = inner
+                        input_args += [k]
+
+                    # Run the actual test
                     s_time = time.time()
-                    func(*run[key]["args"])
+                    func(*input_args)
                     e_time = time.time()
+
                     curr_time = e_time - s_time
                     timings[num_core] += curr_time
         except Exception as e:
+            raise e
             print("Could not run", key, e)
             continue
 
@@ -138,6 +153,120 @@ def main():
 
             string = "%-36s %8.2f %8.2f %8.2f %%" % (name, timings[1], timings[args.num_cores], scaling * 100)
         print(string)
+
+
+def get_shape(obj):
+    shape = None
+    try:
+        shape = obj.shape
+    except Exception as e:
+        pass
+    try:
+        shape = obj.size()
+    except Exception as e:
+        pass
+    return shape
+
+
+class Cache:
+    """Stores data in a cache:
+    cache[object type][hashable key]
+    """
+    def __init__(self):
+        self.cache = dict()
+
+    def has(self, obj):
+        if type(obj) not in self.cache:
+            return False
+        s = obj in self.cache[type(obj)]
+        return s
+
+    def add(self, obj):
+        """Adds object to cache and return the computed value of the object. If it already exists,
+        the computed object is returned"""
+        if not self.has(obj):
+            if not issubclass(type(obj), Computable):
+                raise ValueError("Only Computable objects can be added to cache")
+
+            if type(obj) not in self.cache:
+                self.cache[type(obj)] = dict()
+
+            ret = obj.compute()
+            self.cache[type(obj)][obj] = ret
+        else:
+            ret = self.cache[type(obj)][obj]
+
+        return ret
+
+
+class Computable:
+    def compute(self):
+        raise NotImplementedError()
+
+    def __neq__(self, other):
+        return self != other
+
+class Input(Computable):
+    def __init__(self, shape, scaling, sort_axis=None, random=False):
+        self.shape = shape
+        self.scaling = scaling
+        self.sort_axis = sort_axis
+        self.random = random
+
+    def compute(self):
+        shape = [int(self.shape[0] * self.scaling)] + self.shape[1:]
+        if self.random:
+            output = np.random.rand(*shape)*10
+        else:
+            output = np.zeros(shape)
+
+        if self.sort_axis is not None:
+            output = np.sort(output, axis=self.sort_axis)
+
+        return output
+
+    def __eq__(self, other):
+        return self.shape == other.shape and self.scaling == other.scaling
+
+    def __str__(self):
+        return "Input " + ','.join([str(s) for s in self.shape])
+
+    def __hash__(self):
+        return hash(str(self.shape))
+
+class Grid(Computable):
+    def __init__(self, shape, scaling):
+        self.shape = shape
+        self.scaling = scaling
+
+    def compute(self):
+        return gridpp.Grid(*np.meshgrid(np.linspace(0, 1, self.shape), np.linspace(0, 1, int(self.shape * self.scaling))))
+
+    def __eq__(self, other):
+        return self.shape == other.shape and self.scaling == other.scaling
+
+    def __str__(self):
+        return "Grid " + f"{self.shape}"
+
+    def __hash__(self):
+        return self.shape
+
+class Points(Computable):
+    def __init__(self, shape, scaling):
+        self.shape = shape
+        self.scaling = scaling
+
+    def compute(self):
+        return gridpp.Points(np.random.rand(self.shape), np.random.rand(self.shape))
+
+    def __eq__(self, other):
+        return self.shape == other.shape and self.scaling == other.scaling
+
+    def __str__(self):
+        return "Points " + f"{self.shape}"
+
+    def __hash__(self):
+        return self.shape
 
 
 if __name__ == "__main__":

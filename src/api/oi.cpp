metno/gridpp

commit 6f6a5511eab6cf1266b11aae8aabe3b08b431b7a

Improve speed of OI with variable length scales

src/api/oi.cpp
@@ -234,13 +234,19 @@ vec gridpp::optimal_interpolation_full(const gridpp::Points& bpoints,
 
         // Calculate gridpoint to observation rhos
         lRhos0.reserve(lLocIndices0.size());
+        std::vector<Point> p2;
+        p2.reserve(lLocIndices0.size());
+        for(int i = 0; i < lLocIndices0.size(); i++) {
+            int index = lLocIndices0[i];
+            Point p = points.get_point(index);
+            p2.push_back(p);
+        }
+        vec rhos = structure.corr_background(p1, p2);
         for(int i = 0; i < lLocIndices0.size(); i++) {
             int index = lLocIndices0[i];
             if(gridpp::is_valid(pobs[index]) && gridpp::is_valid(pbackground[index])) {
-                Point p2 = points.get_point(index);
-                float rho = structure.corr_background(p1, p2);
-                if(rho > 0) {
-                    lRhos0.push_back(std::pair<float,int>(rho, i));
+                if(rhos[i] > 0) {
+                    lRhos0.push_back(std::pair<float,int>(rhos[i], i));
                 }
             }
         }
@@ -290,10 +296,15 @@ vec gridpp::optimal_interpolation_full(const gridpp::Points& bpoints,
             lR(i, i) = pratios[index];
             lG(0, i) = lRhos(i);
             Point p1 = points.get_point(index);
+            std::vector<Point> p2(lS, Point(0, 0));
             for(int j = 0; j < lS; j++) {
                 int index_j = lLocIndices[j];
-                Point p2 = points.get_point(index_j);
-                lP(i, j) = structure.corr(p1, p2);
+                Point p = points.get_point(index_j);
+                p2[j] = p;
+            }
+            vec corr = structure.corr(p1, p2);
+            for(int j = 0; j < lS; j++) {
+                lP(i, j) = corr[j];
             }
         }
         mattype lGSR = lG * arma::inv(lP + lR);

src/api/oi_ensi.cpp
@@ -215,13 +215,19 @@ vec2 gridpp::optimal_interpolation_ensi(const gridpp::Points& bpoints,
         std::vector<std::pair<float,int> > lRhos0;
         // Calculate gridpoint to observation rhos
         lRhos0.reserve(lLocIndices0.size());
+        std::vector<Point> p2;
+        p2.reserve(lLocIndices0.size());
+        for(int i = 0; i < lLocIndices0.size(); i++) {
+            int index = lLocIndices0[i];
+            Point p = points.get_point(index);
+            p2.push_back(p);
+        }
+        vec rhos = structure.corr_background(p1, p2);
         for(int i = 0; i < lLocIndices0.size(); i++) {
             int index = lLocIndices0[i];
             if(gridpp::is_valid(pobs[index])) {
-                Point p2 = points.get_point(index);
-                float rho = structure.corr_background(p1, p2);
-                if(rho > 0) {
-                    lRhos0.push_back(std::pair<float,int>(rho, i));
+                if(rhos[i] > 0) {
+                    lRhos0.push_back(std::pair<float,int>(rhos[i], i));
                 }
             }
         }

include/gridpp.h
@@ -13,7 +13,7 @@
 #endif
 #include <exception>
 
-#define GRIDPP_VERSION "0.7.0"
+#define GRIDPP_VERSION "0.8.0.dev1"
 #define __version__ GRIDPP_VERSION
 
 namespace gridpp {
@@ -1900,6 +1900,7 @@ namespace gridpp {
              *  @returns Correlation between points
             */
             virtual float corr(const Point& p1, const Point& p2) const = 0;
+            virtual vec corr(const Point& p1, const std::vector<Point>& p2) const;
 
             /** Correlation between a background point and an observation points
              *  @param p1 Background point
@@ -1907,6 +1908,7 @@ namespace gridpp {
              *  @returns Correlation between background and observation points
             */
             virtual float corr_background(const Point& p1, const Point& p2) const;
+            virtual vec corr_background(const Point& p1, const std::vector<Point>& p2) const;
 
             /** Maximum distance for which an observation can have an impact (localization)
               * @returns Distance [m]
@@ -1966,9 +1968,11 @@ namespace gridpp {
             */
             BarnesStructure(Grid grid, vec2 h, vec2 v, vec2 w, float min_rho=StructureFunction::default_min_rho);
             float corr(const Point& p1, const Point& p2) const;
+            vec corr(const Point& p1, const std::vector<Point>& p2) const;
             StructureFunction* clone() const;
             float localization_distance(const Point& p) const;
         private:
+            float localization_distance(float h) const;
             Grid m_grid;
             vec2 mH;
             vec2 mV;
@@ -1997,6 +2001,7 @@ namespace gridpp {
             CrossValidation(StructureFunction& structure, float dist=MV);
             float corr(const Point& p1, const Point& p2) const;
             float corr_background(const Point& p1, const Point& p2) const;
+            vec corr_background(const Point& p1, const std::vector<Point>& p2) const;
             StructureFunction* clone() const;
             float localization_distance(const Point& p) const;
         private:

src/api/structure.cpp
@@ -10,7 +10,17 @@ gridpp::StructureFunction::StructureFunction(float localization_distance) {
 
     m_localization_distance = localization_distance;
 }
+vec gridpp::StructureFunction::corr(const Point& p1, const std::vector<Point>& p2) const {
+    vec output(p2.size());
+    for(int i = 0; i < p2.size(); i++) {
+        output[i] = corr(p1, p2[i]);
+    }
+    return output;
+}
 float gridpp::StructureFunction::corr_background(const Point& p1, const Point& p2) const {
+    return corr(p1, p2);
+}
+vec gridpp::StructureFunction::corr_background(const Point& p1, const std::vector<Point>& p2) const {
     return corr(p1, p2);
 }
 float gridpp::StructureFunction::barnes_rho(float dist, float length) const {
@@ -108,35 +118,23 @@ gridpp::BarnesStructure::BarnesStructure(Grid grid, vec2 h, vec2 v, vec2 w, floa
 }
 float gridpp::BarnesStructure::corr(const Point& p1, const Point& p2) const {
     float hdist = gridpp::KDTree::calc_straight_distance(p1, p2);
-    if(hdist > localization_distance(p1))
-        return 0;
     float rho = 1;
     if(m_is_spatial) {
+        // This part is slower because of the nearest neighbour lookup
         ivec I = m_grid.get_nearest_neighbour(p1.lat, p1.lon);
         if(I[0] > mH.size())
             throw std::runtime_error("Invalid I[0]");
         if(I[1] > mH[I[0]].size())
             throw std::runtime_error("Invalid I[1]");
-        float h1 = mH[I[0]][I[1]];
-        float v1 = mV[I[0]][I[1]];
-        float w1 = mW[I[0]][I[1]];
-#if 0
-        I = m_grid.get_nearest_neighbour(p2.lat, p2.lon);
-        if(I[0] > mH.size())
-            throw std::runtime_error("Invalid i[0]");
-        if(I[1] > mH[I[0]].size())
-            throw std::runtime_error("Invalid I[1]");
-        float h2 = mH[I[0]][I[1]];
-        float v2 = mV[I[0]][I[1]];
-        float w2 = mW[I[0]][I[1]];
-        float h = (h1 + h2) / 2;
-        float v = (v1 + v2) / 2;
-        float w = (w1 + w2) / 2;
-#else
-        float h = h1;
-        float v = v1;
-        float w = w1;
-#endif
+
+        float h = mH[I[0]][I[1]];
+        float v = mV[I[0]][I[1]];
+        float w = mW[I[0]][I[1]];
+
+        // Use h to compute this, so we don't have to get nearest neighbour twice
+        float loc_dist = localization_distance(h);
+        if(hdist > loc_dist)
+            return 0;
 
         rho = gridpp::StructureFunction::barnes_rho(hdist, h);
         if(gridpp::is_valid(p1.elev) && gridpp::is_valid(p2.elev)) {
@@ -149,6 +147,9 @@ float gridpp::BarnesStructure::corr(const Point& p1, const Point& p2) const {
         }
     }
     else {
+        if(hdist > localization_distance(p1))
+            return 0;
+
         rho = gridpp::StructureFunction::barnes_rho(hdist, mH[0][0]);
         if(gridpp::is_valid(p1.elev) && gridpp::is_valid(p2.elev)) {
             float vdist = p1.elev - p2.elev;
@@ -161,6 +162,43 @@ float gridpp::BarnesStructure::corr(const Point& p1, const Point& p2) const {
     }
     return rho;
 }
+vec gridpp::BarnesStructure::corr(const Point& p1, const std::vector<Point>& p2) const {
+    vec output(p2.size());
+    if(m_is_spatial) {
+        ivec I = m_grid.get_nearest_neighbour(p1.lat, p1.lon);
+        if(I[0] > mH.size())
+            throw std::runtime_error("Invalid I[0]");
+        if(I[1] > mH[I[0]].size())
+            throw std::runtime_error("Invalid I[1]");
+
+        float h = mH[I[0]][I[1]];
+        float v = mV[I[0]][I[1]];
+        float w = mW[I[0]][I[1]];
+        float loc_dist = localization_distance(h);
+        for(int i = 0; i < p2.size(); i++) {
+            float hdist = gridpp::KDTree::calc_straight_distance(p1, p2[i]);
+            float rho = 0;
+            if(hdist <= loc_dist) {
+                rho = gridpp::StructureFunction::barnes_rho(hdist, h);
+                if(gridpp::is_valid(p1.elev) && gridpp::is_valid(p2[i].elev)) {
+                    float vdist = p1.elev - p2[i].elev;
+                    rho *= gridpp::StructureFunction::barnes_rho(vdist, v);
+                }
+                if(gridpp::is_valid(p1.laf) && gridpp::is_valid(p2[i].laf)) {
+                    float lafdist = p1.laf - p2[i].laf;
+                    rho *= gridpp::StructureFunction::barnes_rho(lafdist, w);
+                }
+            }
+            output[i] = rho;
+        }
+    }
+    else {
+        for(int i = 0; i < p2.size(); i++) {
+            output[i] = corr(p1, p2[i]);
+        }
+    }
+    return output;
+}
 gridpp::StructureFunction* gridpp::BarnesStructure::clone() const {
     gridpp::StructureFunction* val = new gridpp::BarnesStructure(m_grid, mH, mV, mW, m_min_rho);
     return val;
@@ -168,13 +206,14 @@ gridpp::StructureFunction* gridpp::BarnesStructure::clone() const {
 float gridpp::BarnesStructure::localization_distance(const Point& p) const {
     if(m_is_spatial) {
         ivec I = m_grid.get_nearest_neighbour(p.lat, p.lon);
-        float curr = sqrt(-2*log(m_min_rho)) * mH[I[0]][I[1]];
-        return curr;
+        return localization_distance(mH[I[0]][I[1]]);
     }
     else {
-        float curr = sqrt(-2*log(m_min_rho)) * mH[0][0];
-        return curr;
+        return localization_distance(mH[0][0]);
     }
+}
+float gridpp::BarnesStructure::localization_distance(float h) const {
+    return sqrt(-2*log(m_min_rho)) * h;
 }
 
 /** Cressman */
@@ -224,6 +263,17 @@ float gridpp::CrossValidation::corr_background(const Point& p1, const Point& p2)
             return 0;
     }
     return m_structure->corr_background(p1, p2);
+}
+vec gridpp::CrossValidation::corr_background(const Point& p1, const std::vector<Point>& p2) const {
+    vec curr_corr = m_structure->corr_background(p1, p2);
+    for(int i = 0; i < curr_corr.size(); i++) {
+        float hdist = gridpp::KDTree::calc_straight_distance(p1, p2[i]);
+        if(gridpp::is_valid(m_dist)) {
+            if(hdist <= m_dist)
+                curr_corr[i] = 0;
+        }
+    }
+    return curr_corr;
 }
 gridpp::StructureFunction* gridpp::CrossValidation::clone() const {
     gridpp::StructureFunction* val = new gridpp::CrossValidation(*m_structure);

tests/benchmark.py
@@ -151,7 +151,15 @@ def main():
             # Compute the fraction between minimum and maximum speedup
             scaling = ((actual_speedup - minimum_speedup) / (maximum_speedup - minimum_speedup))
 
+            # A better way to compute scaling? Computed in time space, instead of in factor space
+            # Probably not a fair way, since scaling will stay fixed for higher number of cores
+            # expected_time = timings[1] / args.num_cores
+            # scaling = 1 - (timings[args.num_cores] - expected_time) / (timings[1] - expected_time)
+
             string = "%-36s %8.2f %8.2f %8.2f %%" % (name, timings[1], timings[args.num_cores], scaling * 100)
+
+            # factor = timings[1] / timings[args.num_cores]
+            # string = "%-36s %8.2f %8.2f %8.2f" % (name, timings[1], timings[args.num_cores], factor)
         print(string)
 
 

commit a4cd72cf3dfeeab9141b80ac30061a122d437a6c

Improve speed of OI
Create Point objects for each observation point once at the beginning of
the function, so that they don't get created many times when passing
into corr().

This makes up for the lost time caused by 6f6a551, where there is extra
overhead dealing with the vector version of corr(). However, with this
fix, OI is even faster than v0.7.0 (about 5%).

src/api/oi.cpp
@@ -211,6 +211,13 @@ vec gridpp::optimal_interpolation_full(const gridpp::Points& bpoints,
     // Compute the background value at observation points (Y)
     vec gY = pbackground;
 
+    // Create all objects of type Point (to save time on creation later)
+    std::vector<Point> point_vec;
+    point_vec.reserve(nS);
+    for(int i = 0; i < nS; i++) {
+        point_vec.push_back(points.get_point(i));
+    }
+
     #pragma omp parallel for
     for(int y = 0; y < nY; y++) {
         if(!gridpp::is_valid(background[y])) {
@@ -238,8 +245,7 @@ vec gridpp::optimal_interpolation_full(const gridpp::Points& bpoints,
         p2.reserve(lLocIndices0.size());
         for(int i = 0; i < lLocIndices0.size(); i++) {
             int index = lLocIndices0[i];
-            Point p = points.get_point(index);
-            p2.push_back(p);
+            p2.push_back(point_vec[index]);
         }
         vec rhos = structure.corr_background(p1, p2);
         for(int i = 0; i < lLocIndices0.size(); i++) {
@@ -295,12 +301,11 @@ vec gridpp::optimal_interpolation_full(const gridpp::Points& bpoints,
             lY(i) = gY[index];
             lR(i, i) = pratios[index];
             lG(0, i) = lRhos(i);
-            Point p1 = points.get_point(index);
+            Point p1 = point_vec[index];
             std::vector<Point> p2(lS, Point(0, 0));
             for(int j = 0; j < lS; j++) {
                 int index_j = lLocIndices[j];
-                Point p = points.get_point(index_j);
-                p2[j] = p;
+                p2[j] = point_vec[index_j];
             }
             vec corr = structure.corr(p1, p2);
             for(int j = 0; j < lS; j++) {

src/api/oi_ensi.cpp
@@ -176,6 +176,13 @@ vec2 gridpp::optimal_interpolation_ensi(const gridpp::Points& bpoints,
         gYhat[i] = mean;
     }
 
+    // Create all objects of type Point (to save time on creation later)
+    std::vector<Point> point_vec;
+    point_vec.reserve(nS);
+    for(int i = 0; i < nS; i++) {
+        point_vec.push_back(points.get_point(i));
+    }
+
     // Calculate number of valid members
     int nValidEns = 0;
     ivec validEns;
@@ -219,8 +226,7 @@ vec2 gridpp::optimal_interpolation_ensi(const gridpp::Points& bpoints,
         p2.reserve(lLocIndices0.size());
         for(int i = 0; i < lLocIndices0.size(); i++) {
             int index = lLocIndices0[i];
-            Point p = points.get_point(index);
-            p2.push_back(p);
+            p2.push_back(point_vec[index]);
         }
         vec rhos = structure.corr_background(p1, p2);
         for(int i = 0; i < lLocIndices0.size(); i++) {
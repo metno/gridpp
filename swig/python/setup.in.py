c0ae1811fe7c2ffac2e51aa107fae0ecd203a279
Thomas Nipen <tnipen@gmail.com>
2024-11-27 09:03:20 +0100

    Don't build pp38 wheels

--- swig/python/setup.in.py
+++ swig/python/setup.in.py
@@ -85,11 +85,11 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
     ],
 
     # What does your project relate to?
